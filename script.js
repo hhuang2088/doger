719b001f57fc695de060023375e5c4a63e98f66b
Henry Huang <hhuang2088@gmail.com>
2014-07-27 17:10:50 -0700

    ajax request is done with the right URL

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ $(document).ready(function() {
   var counter = 0;
   $('#counter').html(counter.toString());
   $('#slider').click(function() {
-    counter += 1;
-    $.get("http://dogecoinerme.appspot.com/oneclick.jsp", function(data) {
-      alert(data.trim);
-    }
-    $('#plus').css({visibility: 'visible'});
 
-    if(counter>=1) {
-      $('#icon').animate({marginLeft:"0px"}, 200);
-      $('#slider').css({backgroundColor:"rgb(0, 255, 0)"});
-      $('#icon').css({pointerEvents:"auto"});
-    } else {
-      $('#icon').animate({marginLeft:'38px'}, 200);
-      $('#slider').css({backgroundColor:"grey"});
-      $('#icon').css({pointerEvents:"none"});
-    }
+    $.get("http://dogecoinerme.appspot.com/oneclick.jsp?uid=sol&domain=block.io", function(data) {
+      counter = parseInt(data.trim(), 10);
+      $('#counter').html(counter.toString());
+      $('#plus').css({visibility: 'visible'});
+      if(counter>=1) {
+        $('#icon').animate({marginLeft:"0px"}, 200);
+        $('#slider').css({backgroundColor:"rgb(0, 255, 0)"});
+        $('#icon').css({pointerEvents:"auto"});
+      } else {
+        $('#icon').animate({marginLeft:'38px'}, 200);
+        $('#slider').css({backgroundColor:"grey"});
+        $('#icon').css({pointerEvents:"none"});
+      }
+    });
+    
   });
   $('#icon').click(function() {
     counter -= 2;
@@ -25,23 +26,4 @@ $(document).ready(function() {
   $('#plus').click(function() {
     $('#counter').html(counter.toString());
   });
-  // $.get({
-  //   url: "http://dogecoinerme.appspot.com/total",
-  //   data: {
-  //     uid: "sol",
-  //     domain: "block.io"
-  //   },
-  //   success: success,
-  //   dataType: jsp
-  // });
-
-  // $.post({
-  //   url: "http://dogecoinerme.appspot.com/oneclick",
-  //   data: {
-  //     uid: "sol",
-  //     domain: "sochain"
-  //   },
-  //   success: success,
-  //   dataType: json
-  // });
 })
