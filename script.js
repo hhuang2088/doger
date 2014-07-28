d320bdd8f8fa3f00ac2aa4c87e33ae7de05254a6
Henry Huang <hhuang2088@gmail.com>
2014-07-27 18:38:38 -0700

    counter now dissappears on hover

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ $(document).ready(function() {
     $.get("http://dogecoinerme.appspot.com/oneclick.jsp?uid=sol&domain=block.io", function(data) {
       counter = parseInt(data.trim(), 10);
       $('#counter').html(counter.toString());
-      $('#plus').css({visibility: 'visible'});
       if(counter>=1) {
         $('#icon').animate({marginLeft:"0px"}, 200);
         $('#slider').css({backgroundColor:"rgb(0, 255, 0)"});
         $('#icon').css({pointerEvents:"auto"});
+        $('#counter').css({visibility: 'visible'});
+        $('#plus').css({visibility: 'visible'});
       } else {
         $('#icon').animate({marginLeft:'38px'}, 200);
         $('#slider').css({backgroundColor:"grey"});
@@ -20,10 +21,14 @@ $(document).ready(function() {
     
   });
   $('#icon').click(function() {
-    counter -= 2;
     $('#counter').html(counter.toString());
   });
   $('#plus').click(function() {
     $('#counter').html(counter.toString());
   });
+  $('#icon').hover(function(){
+    $('#counter').css({visibility: 'hidden'});
+  },function(){
+    $('#counter').css({visibility: 'visible'})
+  })
 })
